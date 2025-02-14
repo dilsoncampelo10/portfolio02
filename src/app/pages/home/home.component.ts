7a924690602a2d8e70eed7a108b4e1ff1f84e51c
Dilson <dilsono316@gmail.com>
2025-02-13 23:06:05 -0300

    init skills area

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/home/header/header.component';
+import { SkillComponent } from '../../components/home/skill/skill.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,SkillComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
