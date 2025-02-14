f159d2b169b9de0bbfd577c9e99789b86359a5e4
Dilson <dilsono316@gmail.com>
2025-02-14 12:16:33 -0300

    fix header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/home/header/header.component';
 import { SkillComponent } from '../../components/home/skill/skill.component';
+import { ExperienceComponent } from '../../components/home/experience/experience.component';
 
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent,SkillComponent],
+  imports: [HeaderComponent,SkillComponent,ExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
