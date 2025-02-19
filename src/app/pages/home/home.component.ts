0330b6df8771cf71807e5d1d87cad4323b963362
Dilson <dilsono316@gmail.com>
2025-02-19 18:08:49 -0300

    init projects

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { SkillComponent } from '../../components/home/skill/skill.component';
 import { ExperienceComponent } from '../../components/home/experience/experience.component';
 import { ServiceComponent } from '../../components/home/service/service.component';
 import { AboutComponent } from '../../components/home/about/about.component';
+import { ProjectComponent } from '../../components/home/project/project.component';
 
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent,SkillComponent,ExperienceComponent, ServiceComponent,AboutComponent],
+  imports: [HeaderComponent,SkillComponent,ExperienceComponent, ServiceComponent,AboutComponent,ProjectComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
